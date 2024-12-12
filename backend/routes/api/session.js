5c769f23b2e2d8448b27239f233df6105210ee72
adilpat998 <adilpathan541@gmail.com>
2024-12-12 17:34:30 -0500

    Add User login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -7,4 +7,71 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
+router.post(
+    '/',
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+  
+      const user = await User.unscoped().findOne({
+        where: {
+          [Op.or]: {
+            username: credential,
+            email: credential
+          }
+        }
+      });
+  
+      if (!user || !bcrypt.compareSync(password, user.hashedPassword.toString())) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+      }
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+    await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
 module.exports = router;
+
+/*
+
+fetch('/api/session', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `lOsguxWk-QMvTou0kx53YsKCvIZpZMvrOW1M`
+  },
+  body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+}).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/session', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `lOsguxWk-QMvTou0kx53YsKCvIZpZMvrOW1M`
+  },
+  body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+}).then(res => res.json()).then(data => console.log(data));
+
+fetch('/api/session', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `lOsguxWk-QMvTou0kx53YsKCvIZpZMvrOW1M`
+  },
+  body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
+}).then(res => res.json()).then(data => console.log(data));
+
+*/
