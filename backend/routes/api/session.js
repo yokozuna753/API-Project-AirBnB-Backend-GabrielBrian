d490b023925576e71aef3acae6fc5d2b09893e45
yokozuna753 <brian894526@gmail.com>
2024-12-12 18:31:59 -0500

    finished the session feature

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -53,6 +53,24 @@ router.delete(
     }
 );
 
+
+router.get(
+  '/',
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+      return res.json({
+        user: safeUser
+      });
+    } else return res.json({ user: null });
+  }
+);
+
 module.exports = router;
 
 /*
