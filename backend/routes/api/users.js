93cca291eced215ec73cd9c54fba5e0765188e0d
adilpat998 <adilpathan541@gmail.com>
2024-12-12 18:17:08 -0500

    signup done

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,49 @@
-const express = require('express')
+const express = require('express');
+const bcrypt = require('bcryptjs');
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
+
 const router = express.Router();
 
-module.exports = router;
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const hashedPassword = bcrypt.hashSync(password);
+      const user = await User.create({ email, username, hashedPassword });
+  
+      const safeUser = {
+        id: user.id,
+        email: user.email,
+        username: user.username,
+      };
+  
+      await setTokenCookie(res, safeUser);
+  
+      return res.json({
+        user: safeUser
+      });
+    }
+  );
+
+
+/*
+
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": "zj8s4XHT-fXKiTvB6CJW0hpF8igWyzN-er1w"
+  },
+  body: JSON.stringify({
+    email: 'spidey@spider.man',
+    username: 'Spidey',
+    password: 'password'
+  })
+}).then(res => res.json()).then(data => console.log(data));
+
+*/
+
+
+module.exports = router; 
