a662e1e7f54fa6839c0b5e29d55b80d8e15ee777
yokozuna753 <brian894526@gmail.com>
2024-12-12 18:53:12 -0500

    added validation for users

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -4,28 +4,55 @@ const bcrypt = require('bcryptjs');
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
+const { check } = require('express-validator');
+const { handleValidationErrors } = require('../../utils/validation');
+
 const router = express.Router();
 
+// backend/routes/api/users.js
+// ...
+const validateSignup = [
+  check('email')
+    .exists({ checkFalsy: true })
+    .isEmail()
+    .withMessage('Please provide a valid email.'),
+  check('username')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 4 })
+    .withMessage('Please provide a username with at least 4 characters.'),
+  check('username')
+    .not()
+    .isEmail()
+    .withMessage('Username cannot be an email.'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .isLength({ min: 6 })
+    .withMessage('Password must be 6 characters or more.'),
+  handleValidationErrors
+];
+
+// Sign up
 router.post(
-    '/',
-    async (req, res) => {
-      const { email, password, username } = req.body;
-      const hashedPassword = bcrypt.hashSync(password);
-      const user = await User.create({ email, username, hashedPassword });
-  
-      const safeUser = {
-        id: user.id,
-        email: user.email,
-        username: user.username,
-      };
-  
-      await setTokenCookie(res, safeUser);
-  
-      return res.json({
-        user: safeUser
-      });
-    }
-  );
+  '/',
+  validateSignup,
+  async (req, res) => {
+    const { email, password, username } = req.body;
+    const hashedPassword = bcrypt.hashSync(password);
+    const user = await User.create({ email, username, hashedPassword });
+
+    const safeUser = {
+      id: user.id,
+      email: user.email,
+      username: user.username,
+    };
+
+    await setTokenCookie(res, safeUser);
+
+    return res.json({
+      user: safeUser
+    });
+  }
+);
 
 
 /*
@@ -40,6 +67,22 @@ fetch('/api/users', {
     email: 'spidey@spider.man',
     username: 'Spidey',
     password: 'password'
+  })
+}).then(res => res.json()).then(data => console.log(data));
+
+// !!!!!!!!!!!!
+
+
+fetch('/api/users', {
+  method: 'POST',
+  headers: {
+    "Content-Type": "application/json",
+    "XSRF-TOKEN": `908thIM9-fMML6UR4ey17dpLAeEDfRMTprCE`
+  },
+  body: JSON.stringify({
+    email: 'firestar@spider.man',
+    username: 'Firestar',
+    password: ''
   })
 }).then(res => res.json()).then(data => console.log(data));
 
