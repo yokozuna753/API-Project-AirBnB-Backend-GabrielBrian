407b9431ba4f50e17ff9bc0a60d15eb6651f1e41
yokozuna753 <brian894526@gmail.com>
2024-12-20 17:28:03 -0500

    pluralized the Reviews seeder table name

--- backend/db/seeders/20241217181322-demo-reviews.js
+++ backend/db/seeders/20241217181322-demo-reviews.js
@@ -35,7 +35,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    options.tableName = 'Review';
+    options.tableName = 'Reviews';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
       spotId: { [Op.in]: [1, 2, 3] }
